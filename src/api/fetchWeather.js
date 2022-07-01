1f1d17d4cf06cf70e75a425b29b29515a02674cd
maziyar parsi <mparsi1992@gmail.com>
2022-07-01 14:14:27 +0430

    add search carsd

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,8 +1,15 @@
 import axios from "axios";
 
 const URL= "https://api.openweathermap.org/data/2.5/weather"
-const API_KEY ="6cfa0413b2eeac7a01fbc2178e24e2ed"
-const fetchWeather = async(query)=>{
-    const response = await axios.get(query)
+const API_KEY = "6cfa0413b2eeac7a01fbc2178e24e2ed"
+export const fetchWeather = async(query)=>{
+    const response = await axios.get(URL,{
+        params:{
+            q:query,
+            units:'metric',
+            APPID:API_KEY
+        }
+    })
     const data = await response.data
+    return data
 }
